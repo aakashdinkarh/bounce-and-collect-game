d090b1de7efcb7b423ef598d6c184cc7b5415449
Dinkar <aakashdinkarh@gmail.com>
2024-02-04 21:13:33 +0530

    added event listeners for mobile events, show arrow for mobile, disable scroll on field touchmove

--- scripts/others/handleEnableDisableField.js
+++ scripts/others/handleEnableDisableField.js
@@ -1,16 +1,24 @@
 function makePlaygroundDisable (){
+    // desktop
     field.removeEventListener('click', moveTheBall);
-
+    
     field.removeEventListener('mousemove', showDirectionOfThrow);
-    field.removeEventListener('mousemove', updateStoredMouseCoordinates);
 
     field.removeEventListener('mousedown', handleMouseDown);
     field.removeEventListener('mousedown', showDirectionOfThrow);
-    field.removeEventListener('mousedown', updateStoredMouseCoordinates);
 
     field.removeEventListener('mouseup', handleMouseUp);
-    field.removeEventListener('mouseup', updateStoredMouseCoordinates);
 
+    // mobile
+    field.removeEventListener('touchend', moveTheBall);
+
+    field.removeEventListener('touchmove', showDirectionOfThrow);
+
+    field.removeEventListener('touchstart', handleMouseDown);
+    field.removeEventListener('touchstart', showDirectionOfThrow);
+
+    field.removeEventListener('touchend', handleMouseUp);
+    
     field.style.cursor = 'not-allowed';
 
     isPlaygroundDisabled = true;
@@ -24,17 +32,25 @@ function makePlaygroundEnable(){
         return;
     }
 
+    // desktop
     field.addEventListener('click', moveTheBall);
 
     field.addEventListener('mousemove', showDirectionOfThrow);
-    field.addEventListener('mousemove', updateStoredMouseCoordinates);
 
     field.addEventListener('mousedown', handleMouseDown);
     field.addEventListener('mousedown', showDirectionOfThrow);
-    field.addEventListener('mousedown', updateStoredMouseCoordinates);
 
     field.addEventListener('mouseup', handleMouseUp);
-    field.addEventListener('mouseup', updateStoredMouseCoordinates);
+
+    // mobile
+    field.addEventListener('touchend', moveTheBall, { passive: true });
+
+    field.addEventListener('touchmove', showDirectionOfThrow, { passive: false });
+
+    field.addEventListener('touchstart', handleMouseDown, { passive: true });
+    field.addEventListener('touchstart', showDirectionOfThrow, { passive: true });
+
+    field.addEventListener('touchend', handleMouseUp, { passive: true });
 
     field.style.cursor = '';
 
